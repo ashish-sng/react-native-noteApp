58726d06bb92bfba36953e966d0d793d02de26a9
Ashish Singh <ashishsingh.1899@gmail.com>
2022-11-19 00:51:18 +0530

    textModal and columnLayout

--- App.js
+++ App.js
@@ -8,12 +8,15 @@ export default function App() {
   const [user, setUser] = useState({});
   const findUser = async () => {
     const result = await AsyncStorage.getItem("user");
-    setUser(JSON.parse(result));
+    if (result !== null) {
+      setUser(JSON.parse(result));
+    }
   };
 
   useEffect(() => {
     findUser();
   }, []);
+  if (!user.name) return <Intro onFinish={findUser} />;
   return <NoteScreen user={user} />;
 }
 
--- app/screens/NoteScreen.js
+++ app/screens/NoteScreen.js
@@ -1,11 +1,24 @@
-import { StatusBar, StyleSheet, Text, View } from "react-native";
+import {
+  FlatList,
+  Keyboard,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TouchableWithoutFeedback,
+  View,
+} from "react-native";
 import React, { useEffect, useState } from "react";
 import colors from "../misc/colors";
 import SearchBar from "../components/SearchBar";
 import RoundIconBtn from "../components/RoundIconBtn";
+import NoteInputModal from "../components/NoteInputModal";
+import Note from "../components/Note";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const NoteScreen = ({ user }) => {
   const [greet, setGreet] = useState("Evening");
+  const [modalVisible, setModalVisible] = useState(false);
+  const [notes, setNotes] = useState([]);
 
   const findGreet = () => {
     const hrs = new Date().getHours();
@@ -14,27 +27,64 @@ const NoteScreen = ({ user }) => {
     setGreet("Evening");
   };
 
+  const handleOnSubmit = async (title, desc) => {
+    const note = { id: Date.now(), title, desc, time: Date.now() };
+    const updatedNotes = [...notes, note];
+    setNotes(updatedNotes);
+    await AsyncStorage.setItem("notes", JSON.stringify(updatedNotes));
+  };
+
+  const findNotes = async () => {
+    const result = await AsyncStorage.getItem("notes");
+    if (result !== null) setNotes(JSON.parse(result));
+  };
+
   useEffect(() => {
+    findNotes();
     findGreet();
   }, []);
 
   return (
     <>
       <StatusBar barStyle="dark-content" backgroundColor={colors.LIGHT} />
-      <View style={styles.container}>
-        <Text style={styles.header}>{`Good ${greet} ${user.name}`}</Text>
-        <SearchBar containerStyle={{ marginVertical: 15 }} />
-        <View
-          style={[StyleSheet.absoluteFillObject, styles.emptyHeaderContainer]}
-        >
-          <Text style={styles.emptyHeader}>Add Notes</Text>
-          <RoundIconBtn
-            onPress={() => console.log("gello")}
-            antIconName="plus"
-            style={styles.addBtn}
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View style={styles.container}>
+          <Text style={styles.header}>{`Good ${greet} ${user.name}`}</Text>
+          {notes.length !== 0 && (
+            <SearchBar containerStyle={{ marginVertical: 15 }} />
+          )}
+          <FlatList
+            data={notes}
+            numColumns={2}
+            columnWrapperStyle={{
+              justifyContent: "space-between",
+              marginBottom: 50,
+            }}
+            keyExtractor={(item) => item.id.toString()}
+            renderItem={({ item }) => <Note item={item} />}
           />
+          {!notes.length && (
+            <View
+              style={[
+                StyleSheet.absoluteFillObject,
+                styles.emptyHeaderContainer,
+              ]}
+            >
+              <Text style={styles.emptyHeader}>Add Notes</Text>
+            </View>
+          )}
         </View>
-      </View>
+      </TouchableWithoutFeedback>
+      <RoundIconBtn
+        onPress={() => setModalVisible(true)}
+        antIconName="plus"
+        style={styles.addBtn}
+      />
+      <NoteInputModal
+        visible={modalVisible}
+        onClose={() => setModalVisible(false)}
+        onSubmit={handleOnSubmit}
+      />
     </>
   );
 };
@@ -49,6 +99,7 @@ const styles = StyleSheet.create({
   container: {
     paddingHorizontal: 20,
     flex: 1,
+    zIndex: 1,
   },
   emptyHeader: {
     fontSize: 30,
