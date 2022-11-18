d74047e73481e511f6c05afe49b6d98d99ed5ae1
Ashish Singh <ashishsingh.1899@gmail.com>
2022-11-18 21:47:09 +0530

    storing and greeting user

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useEffect, useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import Intro from "./app/screens/Intro";
+import NoteScreen from "./app/screens/NoteScreen";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [user, setUser] = useState({});
+  const findUser = async () => {
+    const result = await AsyncStorage.getItem("user");
+    setUser(JSON.parse(result));
+  };
+
+  useEffect(() => {
+    findUser();
+  }, []);
+  return <NoteScreen user={user} />;
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ app/screens/Intro.js
@@ -0,0 +1,70 @@
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  StatusBar,
+  Dimensions,
+} from "react-native";
+import React, { useState } from "react";
+import colors from "../misc/colors";
+import RoundIconBtn from "../components/RoundIconBtn";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const Intro = () => {
+  const [name, setName] = useState("");
+  const handleOnChangeText = (text) => setName(text);
+
+  const handleSubmit = async () => {
+    const user = {name: name} 
+    await AsyncStorage.setItem('user', JSON.stringify(user))
+  }
+
+  return (
+    <>
+      <StatusBar hidden />
+      <View style={styles.container}>
+        <Text style={styles.inputTitle}>Enter your Name to continue</Text>
+        <TextInput
+          value={name}
+          onChangeText={handleOnChangeText}
+          placeholder="Enter Name"
+          style={styles.textInput}
+        />
+        {name.trim().length >= 3 ? (
+          <RoundIconBtn antIconName="arrowright" onPress={handleSubmit} />
+        ) : null}
+      </View>
+    </>
+  );
+};
+
+export default Intro;
+
+const width = Dimensions.get("window").width - 50;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  textInput: {
+    borderWidth: 2,
+    borderColor: colors.PRIMARY,
+    width,
+    height: 50,
+    borderRadius: 10,
+    paddingLeft: 15,
+    fontSize: 25,
+    color: colors.PRIMARY,
+    marginBottom: 15,
+  },
+  inputTitle: {
+    alignSelf: "flex-start",
+    paddingLeft: 25,
+    marginBottom: 5,
+    opacity: 0.5,
+  },
+});
